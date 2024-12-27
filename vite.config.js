a743a967462c8f347605b6e05783d5b6aeed565a
HeidiSiao <heidi.siao@gmail.com>
2024-12-28 02:45:59 +0800

    build environment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Practice-vue-202412/", // 替換為你的儲存庫名稱
+  build: {
+    outDir: "dist", // 編譯結果的輸出目錄
+  },
+  server: {
+    open: true, // 自動打開瀏覽器
+  },
   plugins: [vue()],
-})
+});
