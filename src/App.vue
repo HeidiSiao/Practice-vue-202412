cb8afc9f29d70c242efc5609c70b5d692d5c452a
HeidiSiao <heidi.siao@gmail.com>
2024-12-28 23:36:50 +0800

    漏寫setup/匯入要大寫/使用鉤子

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <test01 v-model="sharedMessage" />
       <h2>這是共享訊息!</h2>
       <p>這邊放了ref的內容：{{ sharedMessage }}</p>
+      <Tiktok />
     </div>
   </div>
 </template>
@@ -13,6 +14,7 @@
 import { ref } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 import test01 from "./components/test01.vue";
+import Tiktok from "./components/Tiktok.vue";
 
 // 定義一個 ref 變數來存儲共享訊息
 const sharedMessage = ref("");
---
+++ src/components/Tiktok.vue
@@ -0,0 +1,24 @@
+<template>
+  <time :datetime="current.toISOString()">
+    目前的時間是：{{ formatTime(current) }}
+  </time>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+const current = ref(new Date());
+const formatTime = (date) => {
+  return date.toLocaleString();
+};
+
+onMounted(() => {
+  const timer = setInterval(() => {
+    current.value = new Date();
+  }, 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(timer);
+});
+</script>
